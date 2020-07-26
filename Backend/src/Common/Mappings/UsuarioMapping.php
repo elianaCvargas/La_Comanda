eeaf614b701c999f668a6f6e7690e46b380c24e8
Acosta Javier Sebastian <acostajavier_ajs@hotmail.com>
2020-07-26 01:30:54 -0300

    cambiado metodo para agregar empleado base

--- Backend/src/Common/Mappings/UsuarioMapping.php
+++ Backend/src/Common/Mappings/UsuarioMapping.php
@@ -1,15 +1,23 @@
 <?php
 namespace Common\Mappings;
 
+use App\Model\Socio;
 use Common\Dto;
 use Model\Empleado;
 
 include_once __DIR__ . '/../../Model/Empleado.php';
+include_once __DIR__ . '/../../Model/Socio.php';
 
 class UsuarioMapping{	
-        public static function ToUser($dto): Empleado
-        {
-                $usuario = new Empleado($dto->nombre, $dto->apellido, $dto->username, $dto->rolEmpleado);
-                return $usuario;
+	public static function ToEmpleado($dto): Empleado
+	{
+		$usuario = new Empleado($dto->nombre, $dto->apellido, $dto->username, $dto->rolEmpleado);
+		return $usuario;
+	}
+
+	public static function ToSocio($dto): Socio
+	{
+		$usuario = new Socio($dto->nombre, $dto->apellido, $dto->username, $dto->rolEmpleado);
+		return $usuario;
 	}
 }
--- Backend/src/Model/socio.php
+++ Backend/src/Model/socio.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 namespace App\Model;
 
 use JsonSerializable;
+use Model\Usuario;
 
-class Socio 
+include_once __DIR__ . '/../../src/Model/usuario.php';
+
+class Socio extends Usuario
 {
-
+  public function __construct($nombre, $apellido, $username)
+  {
+      parent::__construct($nombre, $apellido, $username);
+  }
 }
