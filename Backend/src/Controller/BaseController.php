fe9fe8f9a2b605b9026c7f87d4a91ab69f2db1c2
Acosta Javier Sebastian <acostajavier_ajs@hotmail.com>
2020-07-26 00:36:16 -0300

    ejemplo de uso de validaterequest

--- Backend/src/Controller/BaseController.php
+++ Backend/src/Controller/BaseController.php
@@ -2,19 +2,6 @@
 
 namespace Controller;
 
-use Common\Mappings\UsuarioDtoMapping;
-use PHPUnit\Framework\Exception;
-use Common\ExceptionManager\ApplicationException;
-use Logic\EmpleadoLogic;
-use phpDocumentor\Reflection\Types\Boolean;
-
-include_once __DIR__ . '/../Model/cliente.php';
-include_once __DIR__ . '/../Common/Dto/UsuarioDto.php';
-include_once __DIR__ . '/../Logic/UsuarioLogic.php';
-include_once __DIR__ . '/../Logic/EmpleadoLogic.php';
-include_once __DIR__ . '/../Common/Mappings/UsuarioDtoMapping.php';
-
-
 class BaseController
 {
   public function ValidateRequest(array $datosArray, array $properties)
