9ae5095954f5e8be8ed92c7fdc9b15c2a2ad747a
Acosta Javier Sebastian <acostajavier_ajs@hotmail.com>
2020-07-26 01:04:33 -0300

    agregado socio dto y logic

---
+++ Backend/src/Common/Dto/SocioDto.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Common\Dto;
+
+class SocioDto
+{
+    public $nombre;
+    public $apellido;
+    public $dni;
+    public $telefono;
+    public $rolEmpleado;
+}
